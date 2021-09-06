462654b3da779cb45ef08c40b4eca64c658671a7
Daniel Leite <daniel199257@gmail.com>
2021-09-05 23:46:57 -0300

    Create class DigitalHouseManager

---
+++ Principal/src/Model/DigitalHouseManager.kt
@@ -0,0 +1,7 @@
+package Model
+
+class DigitalHouseManager(
+    var listaDeAlunos: List<Aluno>,
+    var listaDeProfessores: List<Professor>,
+    var listaDeCursos: List<Curso>,
+    var listaDeMatricula: List<Matricula>)
