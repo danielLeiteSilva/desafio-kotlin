ebb9437861884947bb11ff376aca7b569d00ae74
Daniel Leite <daniel199257@gmail.com>
2021-09-05 23:36:50 -0300

    Add new atributes in class curso

--- Principal/src/Model/Curso.kt
+++ Principal/src/Model/Curso.kt
@@ -1,3 +1,10 @@
 package Model
 
-abstract class Curso(nome: String, codigo: Int)
+abstract class Curso(
+    var nome: String,
+    var codigo: Int,
+    var professorTitular: ProfessorTitular,
+    var professorAdjunto: ProfessorAdjunto,
+    var quantidadeMaximaAlunos: Int,
+    var listaDeAlunos: List<Aluno>
+    )
