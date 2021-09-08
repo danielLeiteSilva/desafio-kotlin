c580c4735fe9b8ff79fcf607e48f32e0c981cffc
Daniel Leite <daniel199257@gmail.com>
2021-09-07 21:28:58 -0300

    Add methods in class DigitalHouseManager

--- Principal/src/Model/DigitalHouseManager.kt
+++ Principal/src/Model/DigitalHouseManager.kt
@@ -4,4 +4,38 @@ class DigitalHouseManager(
     var listaDeAlunos: List<Aluno>,
     var listaDeProfessores: List<Professor>,
     var listaDeCursos: List<Curso>,
-    var listaDeMatricula: List<Matricula>)
+    var listaDeMatricula: List<Matricula>){
+
+    fun registrarCurso(nomeCurso: String, codigoCurso: Int, quantidadeMaximaAlunos: Int){
+
+    }
+
+    fun excluirCurso(codigoCurso: Int){
+
+    }
+
+    fun registrarProfessorAdjunto(nome: String,
+                                  sobrenome: String,
+                                  codigoProfessor: Int, quantidadeDeHoras: Int){
+
+    }
+
+    fun registrarProfessorTitular(nome: String, sobrenome: String, codigoProfessor: Int, especialidade: String){
+
+    }
+
+    fun excluirProfessor(codigoProfessor: Int){
+
+    }
+
+    fun matricularAluno(codigoAluno: Int, codigoCurso: Int){
+
+    }
+
+    fun alocarProfessores(codigoCurso: Int,
+                          codigoProfessorTitular: Int, codigoProfessorAdjunto:
+                          Int){
+
+    }
+
+}
